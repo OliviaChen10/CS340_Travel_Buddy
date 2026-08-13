970f70eb774f06a0051b8840b7b415f6b040abe2
Olivia Chen <chenol@classwork.engr.oregonstate.edu>
2026-08-12 17:06:51 -0700

    Added citation for stored procedure code

--- database/PL.sql
+++ database/PL.sql
@@ -1,3 +1,8 @@
+-- ### Citation for implementing CRUD operations
+-- Date retrieved: 8/4/26
+-- Based on the directions/code from Oregon State CS340 Exploration - Implementing CUD operations in your app. Wrote stored procedures and route handler code using node.js instructions.
+-- URL: https://canvas.oregonstate.edu/courses/2051721/pages/exploration-implementing-cud-operations-in-your-app?module_item_id=26923368
+
 -- #############################
 -- RESET to all sample data in our DDL.sql
 -- #############################
